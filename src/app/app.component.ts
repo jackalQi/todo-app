1936c05ea0b4b5c308619996ae5882ee36ed4a36
jackalQi <32143278+jackalQi@users.noreply.github.com>
2018-07-31 12:59:16 -0400

    version 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  clickMessage: string = "";
+  values = "";
+  box2Values = "";
+  values2 = "";
+  values3 = "";
+  arr = ["1","2","3","4","5","6"];
+
+  onClick(){
+    this.clickMessage = "Show me Now";
+  }
+  onClick2(){
+    this.clickMessage = ""
+  }
+
+  onKey(event: KeyboardEvent){
+    this.values += (<HTMLInputElement>event.target).value + '|';
+  }
+
+  onKeyup(value: string){
+    this.box2Values += value + '|';
+  }
+  addNew(value: string){
+    this.arr.push(value);
+  }
+
+  addNew2(value: string){
+    this.arr.push(value)
+    
+  }
 }
---
+++ src/app/site.ts
@@ -0,0 +1,10 @@
+export class Site {
+    constructor(
+        public id:number,
+        public name:string,
+        public url:string,
+        public option?:number,
+    ){
+
+    }
+}
